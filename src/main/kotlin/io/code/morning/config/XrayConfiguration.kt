abbdb8ca0ba604838916ad1065a3e8b020962118
otajisan <taji.taji.otajisan@gmail.com>
2020-04-11 17:10:10 +0900

    :+1: add AWS X-Ray Configuration

---
+++ src/main/kotlin/io/code/morning/config/XrayConfiguration.kt
@@ -0,0 +1,20 @@
+package io.code.morning.config
+
+import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class XrayConfiguration {
+    @Bean
+    fun awsxRayServletFilter() = FilterRegistrationBean<AWSXRayServletFilter>().apply {
+        val registration = FilterRegistrationBean<AWSXRayServletFilter>()
+        registration.filter = AWSXRayServletFilter()
+        registration.order = 1
+        registration.addUrlPatterns("*")
+        registration.setName("XRayServletFilter")
+        registration.addInitParameter("fixedName", "morning-code-blog-service")
+        return registration
+    }
+}
